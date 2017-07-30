159a8163b307ec873e6a1d18d72ebae584e31be6
waldyreyes <151806@p.uapa.edu.do>
2017-07-30 18:04:50 -0400

    crud nomina funcionando

--- crud empleado o modificar etc/CRUDDetalleNomina.Designer.cs
+++ crud empleado o modificar etc/CRUDDetalleNomina.Designer.cs
@@ -30,20 +30,26 @@
         {
             this.components = new System.ComponentModel.Container();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
-            this.btnCrear = new System.Windows.Forms.Button();
+            this.btnGenerarNomina = new System.Windows.Forms.Button();
             this.dgvEmpleado = new System.Windows.Forms.Button();
-            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnModificarNomina = new System.Windows.Forms.Button();
             this.btnBorrar = new System.Windows.Forms.Button();
             this.dgvNomina = new System.Windows.Forms.DataGridView();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.cmbEstatus = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.txtNumeroNomina = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtTotalNomina = new System.Windows.Forms.TextBox();
+            this.txtSS = new System.Windows.Forms.TextBox();
             this.groupBox4.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvNomina)).BeginInit();
             this.groupBox2.SuspendLayout();
@@ -53,54 +59,55 @@
             // 
             // groupBox4
             // 
-            this.groupBox4.Controls.Add(this.btnCrear);
+            this.groupBox4.Controls.Add(this.btnGenerarNomina);
             this.groupBox4.Controls.Add(this.dgvEmpleado);
-            this.groupBox4.Controls.Add(this.btnGuardar);
+            this.groupBox4.Controls.Add(this.btnModificarNomina);
             this.groupBox4.Controls.Add(this.btnBorrar);
             this.groupBox4.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox4.ForeColor = System.Drawing.Color.Blue;
             this.groupBox4.Location = new System.Drawing.Point(856, 50);
             this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(142, 438);
+            this.groupBox4.Size = new System.Drawing.Size(188, 438);
             this.groupBox4.TabIndex = 23;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "Botones de Control";
             // 
-            // btnCrear
+            // btnGenerarNomina
             // 
-            this.btnCrear.Location = new System.Drawing.Point(18, 52);
-            this.btnCrear.Name = "btnCrear";
-            this.btnCrear.Size = new System.Drawing.Size(100, 29);
-            this.btnCrear.TabIndex = 18;
-            this.btnCrear.Text = "Nuevo";
-            this.btnCrear.UseVisualStyleBackColor = true;
+            this.btnGenerarNomina.Location = new System.Drawing.Point(18, 52);
+            this.btnGenerarNomina.Name = "btnGenerarNomina";
+            this.btnGenerarNomina.Size = new System.Drawing.Size(140, 29);
+            this.btnGenerarNomina.TabIndex = 18;
+            this.btnGenerarNomina.Text = "Generar Nomina";
+            this.btnGenerarNomina.UseVisualStyleBackColor = true;
+            //this.btnGenerarNomina.Click += new System.EventHandler(this.btnGenerarNomina_Click);
             // 
             // dgvEmpleado
             // 
             this.dgvEmpleado.Font = new System.Drawing.Font("Monotype Corsiva", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dgvEmpleado.Location = new System.Drawing.Point(18, 336);
             this.dgvEmpleado.Name = "dgvEmpleado";
-            this.dgvEmpleado.Size = new System.Drawing.Size(100, 36);
+            this.dgvEmpleado.Size = new System.Drawing.Size(140, 36);
             this.dgvEmpleado.TabIndex = 21;
             this.dgvEmpleado.Text = "Imprimir";
             this.dgvEmpleado.UseVisualStyleBackColor = true;
             // 
-            // btnGuardar
+            // btnModificarNomina
             // 
-            this.btnGuardar.Enabled = false;
-            this.btnGuardar.Location = new System.Drawing.Point(18, 129);
-            this.btnGuardar.Name = "btnGuardar";
-            this.btnGuardar.Size = new System.Drawing.Size(100, 33);
-            this.btnGuardar.TabIndex = 19;
-            this.btnGuardar.Text = "Guardar";
-            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnModificarNomina.Enabled = false;
+            this.btnModificarNomina.Location = new System.Drawing.Point(18, 129);
+            this.btnModificarNomina.Name = "btnModificarNomina";
+            this.btnModificarNomina.Size = new System.Drawing.Size(140, 33);
+            this.btnModificarNomina.TabIndex = 19;
+            this.btnModificarNomina.Text = "Modificar ";
+            this.btnModificarNomina.UseVisualStyleBackColor = true;
             // 
             // btnBorrar
             // 
             this.btnBorrar.Enabled = false;
             this.btnBorrar.Location = new System.Drawing.Point(18, 223);
             this.btnBorrar.Name = "btnBorrar";
-            this.btnBorrar.Size = new System.Drawing.Size(100, 37);
+            this.btnBorrar.Size = new System.Drawing.Size(140, 37);
             this.btnBorrar.TabIndex = 20;
             this.btnBorrar.Text = "Eliminar";
             this.btnBorrar.UseVisualStyleBackColor = true;
@@ -108,31 +115,33 @@
             // dgvNomina
             // 
             this.dgvNomina.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvNomina.Location = new System.Drawing.Point(28, 22);
+            this.dgvNomina.Location = new System.Drawing.Point(6, 22);
             this.dgvNomina.Name = "dgvNomina";
-            this.dgvNomina.Size = new System.Drawing.Size(777, 190);
+            this.dgvNomina.Size = new System.Drawing.Size(825, 190);
             this.dgvNomina.TabIndex = 24;
+            this.dgvNomina.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgvNomina_CellMouseClick);
             // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.dgvNomina);
-            this.groupBox2.Location = new System.Drawing.Point(31, 149);
+            this.groupBox2.Location = new System.Drawing.Point(13, 150);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(837, 219);
             this.groupBox2.TabIndex = 25;
             this.groupBox2.TabStop = false;
             // 
-            // comboBox1
+            // cmbEstatus
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
+            this.cmbEstatus.FormattingEnabled = true;
+            this.cmbEstatus.Items.AddRange(new object[] {
             "Nuevo",
             "En Proceso"});
-            this.comboBox1.Location = new System.Drawing.Point(73, 27);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 26;
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.cmbEstatus.Location = new System.Drawing.Point(73, 27);
+            this.cmbEstatus.Name = "cmbEstatus";
+            this.cmbEstatus.Size = new System.Drawing.Size(121, 21);
+            this.cmbEstatus.TabIndex = 26;
+            this.cmbEstatus.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.cmbEstatus.MouseClick += new System.Windows.Forms.MouseEventHandler(this.comboBox1_MouseClick);
             // 
             // label1
             // 
@@ -146,7 +155,7 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.cmbEstatus);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Location = new System.Drawing.Point(59, 31);
             this.groupBox1.Name = "groupBox1";
@@ -157,27 +166,35 @@
             // 
             // groupBox3
             // 
-            this.groupBox3.Controls.Add(this.dateTimePicker1);
+            this.groupBox3.Controls.Add(this.textBox2);
+            this.groupBox3.Controls.Add(this.textBox1);
             this.groupBox3.Controls.Add(this.label3);
             this.groupBox3.Controls.Add(this.label2);
             this.groupBox3.Location = new System.Drawing.Point(280, 31);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(455, 77);
+            this.groupBox3.Size = new System.Drawing.Size(520, 77);
             this.groupBox3.TabIndex = 29;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Fecha";
             // 
-            // dateTimePicker1
+            // textBox2
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(70, 29);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
-            this.dateTimePicker1.TabIndex = 4;
+            this.textBox2.Location = new System.Drawing.Point(321, 35);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(82, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(326, 27);
+            this.label3.Location = new System.Drawing.Point(280, 38);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(35, 13);
             this.label3.TabIndex = 3;
@@ -192,11 +209,55 @@
             this.label2.TabIndex = 1;
             this.label2.Text = "Desde";
             // 
+            // txtNumeroNomina
+            // 
+            this.txtNumeroNomina.Location = new System.Drawing.Point(133, 127);
+            this.txtNumeroNomina.Name = "txtNumeroNomina";
+            this.txtNumeroNomina.Size = new System.Drawing.Size(100, 20);
+            this.txtNumeroNomina.TabIndex = 30;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 13);
+            this.label4.TabIndex = 31;
+            this.label4.Text = "Numero de Nomina";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(471, 386);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(96, 13);
+            this.label5.TabIndex = 33;
+            this.label5.Text = "Total de la Nomina";
+            // 
+            // txtTotalNomina
+            // 
+            this.txtTotalNomina.Location = new System.Drawing.Point(575, 386);
+            this.txtTotalNomina.Name = "txtTotalNomina";
+            this.txtTotalNomina.Size = new System.Drawing.Size(100, 20);
+            this.txtTotalNomina.TabIndex = 32;
+            // 
+            // txtSS
+            // 
+            this.txtSS.Location = new System.Drawing.Point(353, 126);
+            this.txtSS.Name = "txtSS";
+            this.txtSS.Size = new System.Drawing.Size(100, 20);
+            this.txtSS.TabIndex = 34;
+            // 
             // CRUDDetalleNomina
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1022, 556);
+            this.ClientSize = new System.Drawing.Size(1056, 556);
+            this.Controls.Add(this.txtSS);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtTotalNomina);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtNumeroNomina);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.groupBox2);
@@ -212,24 +273,31 @@
             this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.GroupBox groupBox4;
-        public System.Windows.Forms.Button btnCrear;
+        public System.Windows.Forms.Button btnGenerarNomina;
         public System.Windows.Forms.Button dgvEmpleado;
-        public System.Windows.Forms.Button btnGuardar;
+        public System.Windows.Forms.Button btnModificarNomina;
         public System.Windows.Forms.Button btnBorrar;
-        private System.Windows.Forms.DataGridView dgvNomina;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cmbEstatus;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtNumeroNomina;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtTotalNomina;
+        private System.Windows.Forms.TextBox txtSS;
+        public System.Windows.Forms.DataGridView dgvNomina;
     }
 }
--- crud empleado o modificar etc/CRUDDetalleNomina.cs
+++ crud empleado o modificar etc/CRUDDetalleNomina.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -29,10 +30,13 @@ namespace crud_empleado_o_modificar_etc
 
         private void CRUDDetalleNomina_Load(object sender, EventArgs e)
         {
-            
+            Operacion op = new Operacion();
+            int numero_nomina=0;
+           
             Operacion oper = new Operacion();
+            txtNumeroNomina.Text = Convert.ToString( numero_nomina + 1); 
 
-             dgvNomina.DataSource=   oper.ConsultaConResultado("select * from empleados");
+            dgvNomina.DataSource=   oper.ConsultaConResultado("select a.nombre, a.apellido, a.cedula, b.nombre_cargo, a.sueldo,  (a.sueldo*0.12) isr, (a.sueldo *0.04) ss, (a.sueldo * 0.02) otros, ((a.sueldo*0.12) + (a.sueldo *0.04) + (a.sueldo * 0.02)) total_deducciones, (a.sueldo-(a.sueldo*0.12) - (a.sueldo *0.04) - (a.sueldo * 0.02)) sueldo_neto from empleados a  inner join cargo b where a.cod_cargo=b.cod_cargo");
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -43,6 +47,110 @@ namespace crud_empleado_o_modificar_etc
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void comboBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if(cmbEstatus.Text == "Nuevo")
+            {
+                btnGenerarNomina.Enabled = true;
+            }
+            else
+            {
+                btnGenerarNomina.Enabled = false;
+            }
+        }
+
+        private void btnGenerarNomina_Click(object sender, DataGridView e)
+        {
+            Operacion op = new Operacion();
+            
+            DataGridViewRow rellenar = dgvNomina.Rows[e.NewRowIndex];
+            float sueldo_bruto = 0;
+
+            float Isr = float.Parse(rellenar.Cells["isr"].Value.ToString());
+            float Ss = float.Parse(rellenar.Cells["ss"].Value.ToString());
+            float Otros = float.Parse(rellenar.Cells["otros"].Value.ToString());
+            float Total_deducciones = float.Parse(rellenar.Cells["total_deducciones"].Value.ToString());
+            float Sueldo_neto = float.Parse(rellenar.Cells["sueldo_neto"].Value.ToString());
+            sueldo_bruto = float.Parse(rellenar.Cells["sueldo"].Value.ToString());
+
+            float total_nomina = +sueldo_bruto;
+            txtTotalNomina.Text = Convert.ToString(total_nomina);
+
+
+            op.ConsultasSinResultados("insert into detalle_nomina(isr, ss, otros, total_deducciones, sueldo_neto, total_nomina, num_nomina) values('" + Isr + "', '" + Ss + "', '" + Otros + "', '" + Total_deducciones + "', '" + Sueldo_neto + "', '" + txtTotalNomina.Text + "', '" + txtNumeroNomina.Text + "')");
+
+
+            // DataGridViewRow rellenar = dgvNomina.Rows[e.RowIndex];
+
+
+            /*Operacion op = new Operacion();
+            SQLiteConnection conexion  = new SQLiteConnection("Data Source=C:\\SISTEMAS\\nomina.s3db; Version=3;");
+
+            SQLiteCommand agregar = new SQLiteCommand("insert into detalle_nomina (@isr, @ss, @otros, @total_deducciones)", conexion);
+            conexion.Open();
+
+            
+
+            try
+            {
+                foreach (DataGridViewRow row in dgvNomina.Rows)
+                {
+                    op.ConsultasSinResultados("insert into detalle_nomina (isr, ss, otros, total_deducciones, sueldo_neto, num_nomina, total_nomina) value('"+ row.Cells ["Column1"].Value+"', '"+ row.Cells ["Column2"].Value +"', '" +row.Cells ["Column3"].Value+ "', '"+row.Cells["Column4"].Value+"', '"+row.Cells["Column5"]+"', '"+txtNumeroNomina.Text+"', '' )");
+                    agregar.Parameters.Clear();
+
+                    agregar.Parameters.AddWithValue("@isr", row.Cells[1].Value);
+                    agregar.Parameters.AddWithValue("@ss", row.Cells[2].Value);
+                    agregar.Parameters.AddWithValue("@otros", row.Cells[3].Value);
+                    agregar.Parameters.AddWithValue("@total_deducciones",row.Cells[4].Value);
+                   // agregar.Parameters.AddWithValue("@sueldo_neto", row.Cells["Column5"].Value);
+                    // agregar.Parameters.AddWithValue("@num_nomina", txtNumeroNomina.Text;
+                    // agregar.Parameters.AddWithValue("@total_nomina", );
+
+                    agregar.ExecuteNonQuery();
+
+
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("error al agregar ");
+            }
+            finally
+            {
+                conexion.Close();
+            }
+            */
+        }
+
+        public void dgvNomina_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            Operacion op = new Operacion();
+            DataGridViewRow rellenar = dgvNomina.Rows[e.RowIndex];
+            float sueldo_bruto=0;
+            
+            
+                foreach (DataGridViewRow row in dgvNomina.Rows)
+                {
+
+                    float Isr = float.Parse(rellenar.Cells["isr"].Value.ToString());
+                    float Ss = float.Parse(rellenar.Cells["ss"].Value.ToString());
+                    float Otros = float.Parse(rellenar.Cells["otros"].Value.ToString());
+                    float Total_deducciones = float.Parse(rellenar.Cells["total_deducciones"].Value.ToString());
+                    float Sueldo_neto = float.Parse(rellenar.Cells["sueldo_neto"].Value.ToString());
+                    sueldo_bruto = float.Parse(rellenar.Cells["sueldo"].Value.ToString());
+
+                    float total_nomina = +sueldo_bruto;
+                    txtTotalNomina.Text = Convert.ToString(total_nomina);
+
+
+                    op.ConsultasSinResultados("insert into detalle_nomina(isr, ss, otros, total_deducciones, sueldo_neto, total_nomina, num_nomina) values('" + Isr + "', '" + Ss + "', '" + Otros + "', '" + Total_deducciones + "', '" + Sueldo_neto + "', '" + txtTotalNomina.Text + "', '" + txtNumeroNomina.Text + "')");
+
+                
+            }
+            
         }
     }
 }
